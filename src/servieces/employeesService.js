75c04da332ba24b9b2adac23fadccb89ae7a4970
Ahmed Amin Bakhabou <AhmedaminHD@outlook.com>
2020-06-26 13:36:52 +0200

    DEV2: adding the employees service

---
+++ src/servieces/employeesService.js
@@ -0,0 +1,17 @@
+import http from './httpService';
+import { toast } from 'react-toastify';
+
+export async function getAvailableTimeSlots() {
+    const url = `/getEmployessEndPoint`;
+    try {
+        const response = await http.get(url);
+        return response["data"];
+    }
+    catch (error) {
+        toast.error("NO Data Found");
+        console.error(error);
+        return {
+            "items": []
+        };
+    }
+}
