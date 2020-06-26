46dbd00ea37266adeb9c0d4d71c9d5cbbce28804
Ahmed Amin Bakhabou <AhmedaminHD@outlook.com>
2020-06-26 14:14:58 +0200

    adding csv data

--- src/servieces/employeesService.js
+++ src/servieces/employeesService.js
@@ -1,7 +1,16 @@
 import http from './httpService';
 import { toast } from 'react-toastify';
+import { func } from 'prop-types';
+
+function ReadCSVDATA() {
+    const reader = new FileReader();
+    reader.readAsText('data.csv');
+    console.log(reader.result)
+}
 
 export async function getAvailableTimeSlots() {
+    ReadCSVDATA();
+    return;
     const url = `/getEmployessEndPoint`;
     try {
         const response = await http.get(url);
